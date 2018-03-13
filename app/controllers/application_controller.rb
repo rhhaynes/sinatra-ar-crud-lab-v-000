6bcab3335e7ee4631852199d7ff2cd32b3867843
Randall Harris Haynes <harrishaynes@gmail.com>
2018-03-13 20:47:49 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,39 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
-  get '/' do 
-    
+  
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+  
+  get '/posts/new' do
+    erb :new
+  end
+  
+  get '/posts/:id' do
+    @post = Post.find(params[:id].to_i)
+    erb :show
+  end
+  
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id].to_i)
+    erb :edit
+  end
+  
+  post '/posts' do
+    Post.create({name: params[:name], content: params[:content]})
+    redirect "/posts"
+  end
+  
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id].to_i)
+    Post.destroy(@post.id)
+    erb :delete
+  end
+  
+  patch '/posts/:id' do
+    Post.update(params[:id].to_i,{name: params[:name], content: params[:content]})
+    redirect "/posts/#{params[:id]}"
   end
 end
